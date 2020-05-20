dd7536a2349551c4724f62e6e7fc15a60cf78179
Marina <ivanovicmimi@gmail.com>
2020-05-20 16:20:33 +0200

    methods tests refactored:

--- source/test.py
+++ source/test.py
@@ -9,42 +9,29 @@ sns.set_style("whitegrid")
 sns.despine(left=True, bottom=True)
 sns.set_context("poster", font_scale = .45, rc={"grid.linewidth": 0.8})
 
-def get_real_test_signal():
+def get_real_test_signal(show=True):
     w = np.pi * np.array([-0.4, -0.2, -0.15, 0, 0.15, 0.2, 0.4])
     alpha = np.array([1, 0.7, 1.2, 0.5, 1.2, 0.7, 1])
     sig = Signal(w=w, alpha=alpha, sigma_n=1)
-    sig.plot_spectar(plt)
-
+    sig.plot_spectar(plt) if show else None
     return sig
 
-def get_imag_test_signal():
+def get_imag_test_signal(show=True):
     w = np.pi * np.array([-0.4, -0.26, -0.18, -0.1, 0.15, 0.36])
     sig = Signal(w=w, sigma_n=0.5)
-    sig.plot_spectar(plt)
-    
+    sig.plot_spectar(plt) if show else None
     return sig
 
-def method_results(method):
-    # Real signal
-    sig = get_real_test_signal()
-    method.estimate(sig)
-    method.plot_pseudo_spectrum(plt)
-    method.plot_w(plt)
-    s_real = method.sigma_n
-
-    # Imag signal
-    sig = get_imag_test_signal()
-    method.estimate(sig)
-    method.plot_pseudo_spectrum(plt)
-    method.plot_w(plt)
-    s_imag = method.sigma_n
-
-    # sigma_n estimations
-    print('Estimation for noise std in:\n'
-          '(1) real signal: {}\n'
-          '(2) imag signal: {}'.format(s_real, s_imag)
-    )
+def method_results(method, signals):
+    for sig in signals:
+        method.estimate(sig)
+        method.plot_pseudo_spectrum(plt)
+        method.plot_w(plt)
+        print('Noise std estimation: {:.2f}'.format(method.sigma_n))
 
 if __name__ == '__main__':
+    real_sig = get_real_test_signal()
+    imag_sig = get_imag_test_signal()
+
     ms = MUSIC()
-    method_results(ms)
+    method_results(ms, [real_sig, imag_sig])
